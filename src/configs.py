c8cc1a5c8a7d7af8c5aa9132147d67e8b407a41e
Sayan Das <poke19962008@gmail.com>
2019-10-27 00:40:37 +0700

    updated configs

--- src/configs.py
+++ src/configs.py
@@ -8,11 +8,11 @@ configs = {
 	'TOP_CATEGORIES_DETECTED': 2,
 	'debug': True,
 	'CAT_CLASS': {
-		'batch': 1000,
+		'batch': 2000,
 		'category_thresh': 10,
 		'cat_lower_limit': 1,
 		'cat_upper_limit': 3,
-		'epochs': 1
+		'epochs': 10
 	},
 	'SOLR_HOST': 'x-search-solr-1.qa1-sg.cld:8983',
 	'SOLR_COLL': 'imageSearch'
