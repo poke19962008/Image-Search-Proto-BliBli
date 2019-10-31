25f1b90ecb7fad309855724c66016dfc8f331311
Sayan Das <poke19962008@gmail.com>
2019-10-31 18:07:14 +0700

    embedding to tf serving

--- src/embedding.py
+++ src/embedding.py
@@ -7,7 +7,10 @@ IMAGE_DIM = (380, 380, 3)
 
 
 def get_embedding(image_np):
-	data = requests.post('localhost:9000/v1/models/ImageClassifier:predict', data=image_np.reshape((1, 380, 380, 3)))
+	payload = {
+		isinstance: [image_np.reshape((1, 380, 380, 3))]
+	}
+	data = requests.post('localhost:9000/v1/models/ImageClassifier:predict', json=payload)
 	return data.text
 
 
